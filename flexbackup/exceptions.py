69e6807adb8022412a426eff12f995167f5dbea3
xImAnton <anton.vogelsang@outlook.de>
2021-04-08 10:30:31 +0200

    added typehints to ChunkNotFoundException

--- flexbackup/exceptions.py
+++ flexbackup/exceptions.py
@@ -1,7 +1,10 @@
+from typing import Tuple
+
+
 class ChunkNotFoundException(Exception):
     """
     raised when a mentioned chunk is not present in the current world
     """
-    def __init__(self, msg, chunk):
+    def __init__(self, msg, chunk: Tuple[int, int]):
         super(ChunkNotFoundException, self).__init__(msg)
-        self.chunk = chunk
+        self.chunk: Tuple[int, int] = chunk
