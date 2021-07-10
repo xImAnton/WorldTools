9ca1b42f5faea05a77dfa70e028f249317e0ced9
xImAnton <anton.vogelsang@outlook.de>
2021-07-10 17:50:16 +0200

    added setup.py and LICENSE

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 # WorldTools
-A python module / CLI for modifying minecraft worlds
+A python module for modifying minecraft worlds
 
 ## Features
-
-* [x] Region File Parsing
-* [ ] NBT Implementation
-* [ ] Flexible World Backup Solutions (copy only chunkwise from Backup)
-* [ ] Flexible Player Backup Solutions (copy only specific players/ player data from Backup)
-* [ ] GUI for selecting Chunks
-* [ ] NBT Explorer GUI
+* Region File Parsing
+* NBT Implementation
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_desc = f.read()
+
+
+setuptools.setup(
+    name="worldtools-xImAnton",
+    version="0.1.0",
+    author="xImAnton_",
+    description="A python module for reading and writing Minecraft Worlds",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xImAnton/WorldTools",
+    project_urls={
+        "Bug Tracker": "https://github.com/xImAnton/WorldTools/issues"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "worldtools"},
+    packages=setuptools.find_packages(where="worldtools"),
+    python_requires=">=3.8"
+)
